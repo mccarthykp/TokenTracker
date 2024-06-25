198d294e9ebab95f2c037e4aebb336f96e77c424
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2024-06-25 14:31:19 -0400

    mobile header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header: React.FunctionComponent = () => {
   return (
-    <header className='bg-slate-800 py-4 px-6 md:text-center select-none'>
+    <header className='bg-slate-800 py-2 px-2 md:text-center select-none'>
       <h1 className='text-2xl md:text-4xl text-slate-200'>Crypto Market Tracker</h1>
       <p className='py-4 text-xs md:text-base text-slate-400'>Top 25 most valuable tokens based on market capitalization.</p>
     </header>
