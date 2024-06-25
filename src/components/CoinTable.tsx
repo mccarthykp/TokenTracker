63b1b57a4d3c2032288c1e50ac316eef6fd3255b
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2024-06-25 12:58:45 -0400

    mobile text sizes

--- src/components/CoinTable.tsx
+++ src/components/CoinTable.tsx
@@ -89,7 +89,7 @@ const CoinTable: React.FunctionComponent = () => {
 
   return (
     <section className="text-xs md:text-base">
-      <table className="min-w-[332px]">
+      <table className="min-w-[340px]">
         <thead>
           <tr className="text-slate-300 text-left select-none">
             <th
