c80d35249177277198ea503fbc459c8b6e8cc876
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2024-06-24 21:03:13 -0400

    mobile text sizes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header: React.FunctionComponent = () => {
   return (
-    <div className='my-5 text-center select-none'>
-      <h1 className='text-4xl md:text-2xl text-slate-200'>Crypto Market Tracker</h1>
-      <p className='pt-5 text-base md:text-sm text-slate-400'>Top 25 most valuable tokens based on market capitalization.</p>
+    <div className='my-4 text-center select-none'>
+      <h1 className='text-2xl md:text-4xl text-slate-200'>Crypto Market Tracker</h1>
+      <p className='pt-4 text-sm md:text-base text-slate-400'>Top 25 most valuable tokens based on market capitalization.</p>
     </div>
   );
 };
