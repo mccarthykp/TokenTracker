0969105937530ee802bea4ad235abc7cd4e0c574
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2024-06-26 14:57:55 -0400

    mobile color styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
     <>
-      <Footer className="flex flex-col py-6 px-8 items-center text-left md:text-center select-none dark:bg-slate-800 bg-zinc-950">
+      <footer className="flex flex-col py-8 px-10 items-center text-left md:text-center select-none dark:bg-slate-800 bg-zinc-950">
         <p className="text-xs text-slate-400 min-w-[355px]">Built with React. Hosted on GitHub Pages.</p>
         <p className="text-xs text-slate-400 min-w-[355px]">
           &copy; 2024 - <b>Designed & Developed by Kevin McCarthy</b>
         </p>
-      </Footer>
+      </footer>
     </>
   );
 }
