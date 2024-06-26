ac63698709abb9d98fe5d1f4dc36a57f4d0681f0
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2024-06-26 14:11:04 -0400

    mobile color styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className="py-6 text-xs text-slate-400 select-none pl-5 md:text-center dark:bg-slate-800 bg-zinc-950">
+      <footer className="py-6 text-xs text-slate-400 select-none pl-5 md:text-center dark:bg-slate-800 bg-zinc-950 overflow-hidden">
         <p>Built with React. Hosted on GitHub Pages.</p>
         <p>
           &copy; 2024 - <b>Designed & Developed by Kevin McCarthy</b>
